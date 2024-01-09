553630f9e4442893f0cfdbe67fd53e7e2fc048c7
Eddpoxy <edward.lundh@elev.ga.lbs.se>
2024-01-09 16:12:20 +0100

    movement

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(5, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-5, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, 5));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector2(0, -5));
+        }
+    } 
 }
