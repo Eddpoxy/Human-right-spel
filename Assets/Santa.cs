1187ba51cf7ae50c0dda8e2f08490562a5f7c13c
Eddpoxy <edward.lundh@elev.ga.lbs.se>
2024-01-10 13:47:23 +0100

    Ai (almost done)

--- Assets/Santa.cs
+++ Assets/Santa.cs
@@ -1,34 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Santa : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public float offset;
-    public Transform target;
-    public int speed = 1000;
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+
+ 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+       
+        agent.updateRotation = false;
+        agent.updateUpAxis = false; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Calculate the direction from the current position to the target position
-        Vector2 direction = target.position - transform.position;
-
-        // Normalize the direction vector to have a magnitude of 1
-        direction.Normalize();
-
-        // Calculate the movement vector based on the direction and speed
-        Vector2 movement = direction * speed * Time.deltaTime;
-
-        // Move the object towards the target
-        transform.Translate(movement);
+        agent.SetDestination(target.position);
 
     }
 }
